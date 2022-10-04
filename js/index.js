73d52c9cc7caf1c8579ed2ca3ef5aabadedbf519
Jared Clyde <greatwyrm@comcast.net>
2022-10-04 13:40:50 -0600

    Added Font Awesome

--- js/index.js
+++ js/index.js
@@ -4,10 +4,11 @@ import '../styles/styles.scss';
 import image from '../images/image.jpeg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import '@fortawesome/fontawesome-free/js/all';
 // Display the image
 const imageElement = document.createElement('img');
 imageElement.src = image; 
 imageElement.alt = 'Always provide a descriptive alt text.';
 document.body.append(imageElement);
 // Print the current time
-document.querySelector('.js-content').innerHTML = `It's ${ getDateString() }`;
+document.querySelector('.js-content').innerHTML = `<i class="fas fa-clock"></i> ${ getDateString() }`;
